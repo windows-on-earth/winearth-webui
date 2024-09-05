3d66241f4fddaa117135dc8708aa9780cb4449ce
Alex Nguyen <alex.nguyen369@gmail.com>
2024-09-04 17:48:43 -0700

    Add metadata to movie page and consolidated time related functions into one file

--- src/app/movie/[movie_title]/page.tsx
+++ src/app/movie/[movie_title]/page.tsx
@@ -2,14 +2,24 @@ import SearchBar from '@/app/ui/searchbar'
 import MovieElements from '@/app/ui/movie/movie-elements'
 import { useRouter } from 'next/router'
 import { getMovieData } from '@/actions/getMovieData'
+import MovieData from '@/app/ui/movie/movie-data'
 
 export default async function Page({ params }: { params: { movie_title: string } }) {
   const movieData = await getMovieData(params.movie_title)
   return (
     <main>
-      <div className="flex flex-col relative top-12 left-24 w-3/5">
-        <SearchBar />
-        <MovieElements data={movieData}/>
+      <div className="flex flex-row gap-1 w-11/12 m-auto">
+        <div className="flex flex-col w-3/4">
+          <SearchBar />
+          <MovieElements data={movieData}/>
+        </div>
+        <div className="flex flex-col mt-12 w-1/4">
+          {/* Home/Search Button? */}
+          <div className="border-2 border-purple-400 h-1/4 hidden"></div>
+          <div className="bg-neutral-400 basis-5/6">
+            <MovieData movie={movieData}/>
+          </div>
+        </div>
       </div>
     </main>
   )
---
+++ src/utils/time.ts
@@ -0,0 +1,42 @@
+
+/**
+ * Converts a time expressed in unix to a requested datetime format.
+ * @param unix Time in unix
+ * @param format Date/Time format wanting to be returned. Accepts "Date", "Time", and "Datetime"
+ * @returns `String` formatted datetime according to `format` parameter.
+ */
+export function convertUnixToDatetime (unix: number, format: string) {
+    const unixtime = new Date(Number(unix) * 1000)
+    // Extract date components
+    const month = unixtime.toLocaleString('en-US', { month: 'short', timeZone: 'GMT' });
+    const day = unixtime.toLocaleString('en-US', { day: '2-digit', timeZone: 'GMT' });
+    const year = unixtime.toLocaleString('en-US', { year: 'numeric', timeZone: 'GMT' });
+    const hours = unixtime.toLocaleString('en-US', { hour: '2-digit', hour12: false, timeZone: 'GMT' });
+    const minutes = unixtime.toLocaleString('en-US', { minute: '2-digit', timeZone: 'GMT' });
+    const seconds = unixtime.toLocaleString('en-US', { second: '2-digit', timeZone: 'GMT' });
+
+    switch (format) {
+        case "Date": {
+            return `${month} ${day}, ${year}`
+        }
+        case "Time": {
+            return `${hours}:${minutes}:${seconds} GMT`
+        }
+        case "Datetime": {
+            return `${month} ${day} ${year} ${hours}:${minutes} GMT`
+        }
+        default: {
+            return "Invalid format type. Choose between 'Date', 'Time', or 'Datetime'."
+        }
+    }
+}
+
+export function secondsToHms(d: number) {
+    d = Number(d);
+
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+
+    return ('0' + h).slice(-2) + ":" + ('0' + m).slice(-2) + ":" + ('0' + s).slice(-2);
+}
